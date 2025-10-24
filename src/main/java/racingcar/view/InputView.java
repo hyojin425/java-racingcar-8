95a9142b70b71f273f0f2c1bc326529bdf39221a
hyojin425 <gywls5184@gmail.com>
2025-10-24 16:06:53 +0900

    feat: 시도 횟수 입력 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public String getRepeatCount() {
+        System.out.print("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
 }
