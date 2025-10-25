dbdf1aa0cc59d29c516807a3fbf5baae023cc337
hyojin425 <gywls5184@gmail.com>
2025-10-26 03:05:20 +0900

    feat: 랜덤 숫차 추출하기 추가

---
+++ src/main/java/racingcar/service/RandomGenerator.java
@@ -0,0 +1,10 @@
+package racingcar.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGenerator {
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
