a78f40762f2d96018365165f3de4a0bc510e6276
hyojin425 <gywls5184@gmail.com>
2025-10-26 02:41:30 +0900

    feat: 자동자 이름 구분하기

---
+++ src/main/java/racingcar/service/Parser.java
@@ -0,0 +1,12 @@
+package racingcar.service;
+
+import java.util.List;
+
+public class Parser {
+
+    private final String DELIMITER = ",";
+
+    public List<String> parseCarName(String carName) {
+        return List.of(carName.split("DELIMITER"));
+    }
+}
