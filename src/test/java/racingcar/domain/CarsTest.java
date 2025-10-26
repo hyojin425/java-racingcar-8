c0f1937a516c1d80545e37bed8712f29f5368d29
hyojin425 <gywls5184@gmail.com>
2025-10-26 18:46:37 +0900

    test: Cars 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    @DisplayName("Cars 객체 생성 시 carList 저장한다.")
+    @Test
+    void create_cars() {
+        // given
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+        Cars cars = new Cars(List.of(car1, car2, car3));
+
+        // when
+        List<Car> result = cars.getCarList();
+
+        // then
+        assertThat(result).containsExactly(car1, car2, car3);
+    }
+}
