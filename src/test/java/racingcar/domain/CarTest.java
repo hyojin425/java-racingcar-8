de926750c4d7af929004b627f3b72e3cd6ab18ce
hyojin425 <gywls5184@gmail.com>
2025-10-26 18:41:16 +0900

    test: Car 테스트 코드 작성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CarTest {
+
+    @DisplayName("moveDistance 값이 최소값 이상이면 이동 거리 증가")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void move_test(int randomNumber) {
+        // given
+        Car car = new Car("pobi");
+
+        // when
+        int moveDistance = car.move(randomNumber);
+
+        // then
+        assertThat(moveDistance).isEqualTo(randomNumber);
+    }
+
+    @DisplayName("moveDistance 값이 최소값 이하이면 이동 거리는 그대로")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void not_ove_test(int randomNumber) {
+        // given
+        Car car = new Car("pobi");
+
+        // when
+        int moveDistance = car.move(randomNumber);
+
+        // then
+        assertThat(moveDistance).isEqualTo(0);
+    }
+}
