36c1c7d78c6d0758b0e03fdc053be8f7e93069ab
hyojin425 <gywls5184@gmail.com>
2025-10-26 16:16:10 +0900

    test: CarManager 테스트 코트 추가

---
+++ src/test/java/racingcar/service/CarManagerTest.java
@@ -0,0 +1,54 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.validator.Validator;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarManagerTest {
+
+    private Parser parser;
+    private Validator validator;
+    private CarManager carManager;
+
+    @BeforeEach
+    void setUp() {
+        parser = new Parser();
+        validator = new Validator();
+        carManager = new CarManager(parser, validator);
+    }
+
+    @DisplayName("자동차 이름에 중복이 없는 경우 List<Car>를 생성한다.")
+    @Test
+    void createCarList_WithUniqueNames_ShouldSucceed() {
+        // given
+        String carNames = "pobi,woni,jun";
+
+        // when
+        List<Car> carList = assertDoesNotThrow(() -> carManager.createCarList(carNames));
+
+        // then
+        assertEquals(3, carList.size());
+        assertEquals("pobi", carList.get(0).getName());
+        assertEquals("woni", carList.get(1).getName());
+        assertEquals("jun", carList.get(2).getName());
+    }
+
+    @DisplayName("자동차 이름에 중복이 있는 경우 IllegalArgumentException 발생한다.")
+    @Test
+    void createCarList_WithDuplicateNames_ShouldThrowException() {
+        // given
+        String carNames = "pobi,pobi,jun"; // car1 중복
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> carManager.createCarList(carNames));
+
+        assertEquals("중복된 자동차 이름이 존재합니다: pobi", exception.getMessage());
+    }
+}
