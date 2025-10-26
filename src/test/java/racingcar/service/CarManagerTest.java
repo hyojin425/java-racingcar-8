bb95a2aa30c7f4651dd3ca70e817224320f6d0b9
hyojin425 <gywls5184@gmail.com>
2025-10-27 01:38:53 +0900

    test: 우승자 구하기 테스트 코드 추가

--- src/test/java/racingcar/service/CarManagerTest.java
+++ src/test/java/racingcar/service/CarManagerTest.java
@@ -4,40 +4,44 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
-import racingcar.utils.Parser;
+import racingcar.domain.Cars;
 import racingcar.utils.RandomGenerator;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CarManagerTest {
 
-    private Parser parser;
     private CarManager carManager;
     private RandomGenerator randomGenerator;
 
     @BeforeEach
     void setUp() {
-        parser = new Parser();
         randomGenerator = new RandomGenerator();
         carManager = new CarManager(randomGenerator);
     }
 
-    @DisplayName("자동차 이름에 중복이 없는 경우 List<Car>를 생성한다.")
+    @DisplayName("우승자를 반환한다.")
     @Test
-    void createCarList_ShouldReturnCarList_WhenNamesAreUnique() {
+    void getWinner_ShouldReturnCarsWithMaxDistance() {
         // given
-        List<String> carNames = parser.parseCarName("pobi,woni,jun");
+        Car pobi = new Car("pobi");
+        Car woni = new Car("woni");
+        Car jun = new Car("jun");
+
+        pobi.move(3);
+        woni.move(5);
+        jun.move(5);
+
+        List<Car> carList = List.of(pobi, woni, jun);
+        carManager.createCars(carList);
 
         // when
-        List<Car> carList = assertDoesNotThrow(() -> carManager.createCarList(carNames));
+        Cars winners = carManager.getWinner();
 
         // then
-        assertEquals(3, carList.size());
-        assertEquals("pobi", carList.get(0).getName());
-        assertEquals("woni", carList.get(1).getName());
-        assertEquals("jun", carList.get(2).getName());
+        assertThat(winners.getCarList().get(0).getName()).isEqualTo("woni");
+        assertThat(winners.getCarList().get(1).getName()).isEqualTo("jun");
     }
 }
