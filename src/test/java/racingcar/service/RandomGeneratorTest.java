c6e1816cc69d75eddbfc99635d087c0d4ae0b43d
hyojin425 <gywls5184@gmail.com>
2025-10-26 20:12:33 +0900

    test: RandomGenerator test 코드 추가

---
+++ src/test/java/racingcar/service/RandomGeneratorTest.java
@@ -0,0 +1,27 @@
+package racingcar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class RandomGeneratorTest {
+
+    private RandomGenerator randomGenerator;
+
+    @BeforeEach
+    void setUp() {
+        randomGenerator = new RandomGenerator();
+    }
+
+    @DisplayName("랜덤 숫자가 0 이상 9 이하의 범위를 벗어나지 않아야 한다.")
+    @Test
+    void getRandomNumber_ShouldReturnNumberInRange() {
+        // given & when
+        int number = randomGenerator.getRandomNumber();
+
+        // then
+        assertThat(number).isBetween(0, 9);
+    }
+}
