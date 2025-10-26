6f74c59d1f07ff80e842cb2138fe2bfe49e6c8ab
hyojin425 <gywls5184@gmail.com>
2025-10-26 14:38:18 +0900

    feat: 동일한 이름의 자동차가 입력된 경우 예외처리 추가

--- src/main/java/racingcar/validator/Validator.java
+++ src/main/java/racingcar/validator/Validator.java
@@ -1,5 +1,22 @@
 package racingcar.validator;
 
+import racingcar.domain.Car;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
 
+    public void validateDuplicateNames(List<Car> cars) {
+        Set<String> names = new HashSet<>();
+
+        cars.stream()
+                .map(Car::getName)
+                .filter(name -> !names.add(name))
+                .findFirst()
+                .ifPresent(duplicateName -> {
+                    throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다: " + duplicateName);
+                });
+    }
 }
